c63c7fb9c8aacd16bf9a13939a53ae3671ffeb83
Raiyaad-Raad <raiyaadraad132rafan45@gmail.com>
2021-10-19 10:50:21 +0600

    added

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,6 +3,9 @@ const client = require("../index");
 const embed = new MessageEmbed()
 .setColor("RED")
 .setDescription("I do not have permission to execute this command") 
+const resp = new MessageEmbed()
+.setColor("RED")
+.setDescription("You do not have permission to execute this command") 
 
 client.on("messageCreate", async (message) => {
     if (
@@ -21,9 +24,9 @@ client.on("messageCreate", async (message) => {
 
     if (!command) return;
 
-    if(!message.member.permissions.has(command.permission || [])) return message.channel.send(
-        "You do not have permissions to use this commands"
-    )
+    if(!message.member.permissions.has(command.permission || [])) return message.channel.send({
+        embeds: [resp]
+    })
 
     if(!message.guild.me.permissions.has(command.botpermission || [])) return message.channel.send({ embeds: [embed] })
 
