1dd9e7afed8587994492f194afde7632cedade27
Raiyaad Raad Flash <77395366+Raiyaad-Raad@users.noreply.github.com>
2021-10-20 09:04:18 +0600

    Hgs

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,37 +1,20 @@
-
-const { MessageEmbed } = require("discord.js");
 const client = require("../index");
-const premiumSchema = require("../models/permium");
 
-client.on("messageCreate", async (message) => { 
-if ( 
-message.author.bot || 
-!message.guild || 
-message.content.toLowerCase().startsWith(client.config.prefix) 
-) 
-return; 
+client.on("messageCreate", async (message) => {
+    if (
+        message.author.bot ||
+        !message.guild ||
+        !message.content.toLowerCase().startsWith(client.config.prefix)
+    )
+        return;
 
-const [cmd, ...args] = message.content 
-.slice(client.config.prefix.length) 
-.trim() 
-.split(/ +/g);
- const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
+    const [cmd, ...args] = message.content
+        .slice(client.config.prefix.length)
+        .trim()
+        .split(/ +/g);
 
- if (!command) return;
+    const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
 
-          if(command.premium && !await premiumSchema.findOne({ User: message.author.id })) return message.channel.send(
-        "You don't have premium you need to upgrade to premium in order to use this command"
-   )
-
-    if(!message.member.permissions.has(command.permission || [])) return message.channel.send({
-        embeds: [resp]
-    })
-
-     if(!message.guild.me.permissions.has(command.botpermission || [])) return message.channel.send({ 
-embeds: [embed] 
-})
-
- await command.run(client, message, args);
-
+    if (!command) return;
+    await command.run(client, message, args);
 });
-
