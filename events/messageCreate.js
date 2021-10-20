564abcc64dbf41324ec98b3ebc487960a1655361
Raiyaad Raad Flash <77395366+Raiyaad-Raad@users.noreply.github.com>
2021-10-20 09:06:23 +0600

    Yay

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -16,5 +16,10 @@ client.on("messageCreate", async (message) => {
     const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
 
     if (!command) return;
+
+    if(command.premium && !await premiumSchema.findOne({ User: message.author.id })) return message.channel.send(
+        "You don't have premium you need to upgrade to premium in order to use this command"
+    );
+
     await command.run(client, message, args);
 });
